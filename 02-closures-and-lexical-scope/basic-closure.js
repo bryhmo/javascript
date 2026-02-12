99806a191b36e1d069713daa9b7c1396ddcce171
ibrahim <codewithbryhmo@gmail.com>
2026-02-12 23:30:28 +0100

    function factory Example in closure

--- 02-closures-and-lexical-scope/basic-closure.js
+++ 02-closures-and-lexical-scope/basic-closure.js
@@ -13,3 +13,36 @@ let counter = outer();
 counter();
 counter();
 counter();
+
+
+
+// closure with the secret variable 
+function privateVariable(secret){
+  return function(){
+    console.log(secret);
+  }
+}
+
+const secretVar = privateVariable("Becoming Scary with javascript");
+secretVar();
+secretVar();
+
+
+function multiplyBy(x){
+
+  return function(y){
+
+    // console.log(x * y);
+    return x * y;
+
+  }
+}
+
+const double = multiplyBy(2);
+const tripple = multiplyBy(3);
+
+
+console.log(double(5) );
+console.log(tripple(5));
+
+
