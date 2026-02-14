aa444a4d1208780139277dc05d00e1f1e10ca90a
ibrahim <codewithbryhmo@gmail.com>
2026-02-14 05:14:44 +0100

    Bank account closure ... The balance is not directly accesible but only accessible through the returned functions only and is a closure

--- 02-closures-and-lexical-scope/bank-account-closure.js
+++ 02-closures-and-lexical-scope/bank-account-closure.js
@@ -1,6 +1,42 @@
-function createBankAccount(getInitialBalance){
-  return function(){
-    
-  }
+function createBankAccount(initialBalance){
+  let balance = initialBalance
+  return{
 
+    deposit:function(amount){
+
+      balance+=amount;
+
+      console.log('You deposited :',amount);
+    },
+
+    withdraw:function(amount){
+
+      if(amount>balance){
+
+        console.log("insufficient funds");
+
+      } else {
+
+        balance-=amount;
+
+        console.log("you withdraw :",amount);
+
+      }
+    },
+
+    getBalance:function() {
+
+      console.log("your current balance is :",balance);
+      
+    }
+  }
 }
+
+const account1 = createBankAccount(100);
+
+
+account1.deposit(56);
+account1.getBalance();
+account1.withdraw(21);
+account1.getBalance();
+
