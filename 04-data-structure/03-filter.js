1a1e4f86dc5f4b497bbbdac5ffaf6ddf29c13c9b
bryhmo <codewithbryhmo@gmail.com>
2026-03-18 23:06:57 +0100

    working with filter and map

--- 04-data-structure/03-filter.js
+++ 04-data-structure/03-filter.js
@@ -1,3 +1,13 @@
+
+/* What filter() Really Does
+
+filter():
+Returns a new array containing ONLY the elements that pass a condition.
+It does NOT modify the original array
+
+*/
+
+
 const fit1 = [10,20,14,23,29,19];
 
 const result1 = fit1.filter(num =>num >= 18);
@@ -19,10 +29,8 @@ console.log(result3);
 
 const numbers = [5, 12, 8, 20];
 
-const result = numbers
-    .map(n => n * 2)
-    .filter(n => n > 20);
-
+// this is joining the map function and the filter function together 
+const result = numbers.map(n => n * 2).filter(n => n > 20);
 console.log(result);
 
 const users = [
@@ -33,3 +41,12 @@ const users = [
 const hasAdmin = users.some(u => u.isAdmin);
 
 console.log(hasAdmin);
+
+
+const students = [2,3,4,5,6];
+const stuResult = students.map(student=>student*4);
+console.log(stuResult);
+
+const sortEvenNumbers = [3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+const evenNumbers = sortEvenNumbers.filter(even=>even%2===0).map(even=>even**2);
+console.log(evenNumbers);
